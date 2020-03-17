3839784063eb03caf96feef9cc9271fea805541c
ilkrbk <ilkrbk@gmail.com>
2020-03-17 11:13:51 +0200

    final commit

--- StirSort/Program.cs
+++ StirSort/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace StirSort
 {
@@ -6,7 +7,37 @@ namespace StirSort
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<int> array = new List<int>() {8, 1, 3, 5, 2, 7, 9};
+            for (var i = 0; i < array.Count / 2; i++)
+            {
+                bool swapFlag = false;
+                int temp;
+                // в условии < по убыванию
+                //           > по возростанию
+                for (var j = i; j < array.Count - i - 1; j++)
+                    if (array[j] < array[j + 1])
+                    {
+                        temp = array[j + 1];
+                        array[j + 1] = array[j];
+                        array[j] = temp;
+                        swapFlag = true;
+                    }
+                for (var j = array.Count - i - 1; j > i; j--)
+                    if (array[j - 1] < array[j])
+                    {
+                        temp = array[j - 1];
+                        array[j - 1] = array[j];
+                        array[j] = temp;
+                        swapFlag = true;
+                    }
+                if (!swapFlag)
+                    break;
+            }
+
+            for (int i = 0; i < array.Count; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
         }
     }
 }
